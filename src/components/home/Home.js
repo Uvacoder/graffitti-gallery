2f56b1cefc5b70d01855d87ae4c110d21c14601a
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-10-10 09:58:36 -0400

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,7 +6,7 @@ function Home () {
         <div>
             <div id="introParagraph">
                 <div id="layer">
-                <img id="girlpic" src="https://images.unsplash.com/photo-1561059488-916d69792237?crop=entropy&cs=tinysrgb&fit=max&fm=jpg" width="950px" height="700px"/>
+                <img id="girlpic" src="https://images.unsplash.com/photo-1561059488-916d69792237?crop=entropy&cs=tinysrgb&fit=max&fm=jpg" width="950px" height="700px" alt="" />
                 </div>
             </div>
                
